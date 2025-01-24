35888861fd1247447e7d05daf919e03ff2b20575
Danuka koshitha <danukakoshitha2004@gmail.com>
2025-01-24 17:06:28 +0530

    first commit

---
+++ src/main/java/Frame.java
@@ -0,0 +1,25 @@
+import javafx.event.ActionEvent;
+
+public class Frame {
+    public void btnOrderOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnProductOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUserOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+}
