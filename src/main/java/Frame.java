daa8103b5be1dd8af98c00eb4691754156e89f8c
Danuka koshitha <danukakoshitha2004@gmail.com>
2025-01-25 23:44:03 +0530

    second commit

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,11 +1,33 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
-public class Frame {
-    public void btnOrderOnAction(ActionEvent actionEvent) {
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+public class Frame implements Initializable {
+    public AnchorPane loadfom;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void btnOrderOnAction(ActionEvent actionEvent) throws IOException {
 
     }
 
-    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+    public void loadDashboard() throws IOException {
+        URL url = this.getClass().getResource("View/Dashboard.fxml");
+
+        assert url != null;
+
+        Parent load = FXMLLoader.load(url);
+        this.loadfom.getChildren().clear();
+        this.loadfom.getChildren().add(load);
     }
 
     public void btnSupplierOnAction(ActionEvent actionEvent) {
@@ -17,9 +39,35 @@ public class Frame {
     public void btnUserOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnDashboardOnAction(ActionEvent actionEvent) {
+    public void btnDashboardOnAction(ActionEvent actionEvent) throws IOException {
+        loadDashboard();
     }
 
     public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            loadDashboard();
+            getDate();
+            getTime();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getDate(){
+        LocalDate date = LocalDate.now();
+        String stringDate = date.toString();
+        lblDate.setText(stringDate);
+        return stringDate;
+    }
+
+    public String getTime(){
+        LocalTime time = LocalTime.now();
+        String stringTime = time.toString();
+        lblTime.setText(stringTime);
+        return stringTime;
     }
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+
+public class User {
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
